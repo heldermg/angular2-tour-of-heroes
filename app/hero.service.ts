e125e8a0ab02be887b17304a83a61da4f67ac3ca
Helder Martins Gomes <helder.gomes@serpro.gov.br>
2016-04-08 18:52:38 -0300

    Chapter 6 - Part 1

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -8,9 +8,15 @@ export class HeroService {
     return Promise.resolve(HEROES);
   }
 
+  getHero(id: number) {
+    return Promise.resolve(HEROES)
+      .then(heroes => heroes.filter(hero => hero.id === id)[0]
+    );
+  }
+
   getHeroesSlowly() {
-  return new Promise<Hero[]>(resolve =>
-    setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
-  );
-}
+    return new Promise<Hero[]>(resolve =>
+      setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
+    );
+  }
 }
