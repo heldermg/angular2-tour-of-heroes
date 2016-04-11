516028268d561186d6ea50524179e3dbfa78a097
Helder Martins Gomes <helder.gomes@serpro.gov.br>
2016-04-11 09:43:18 -0300

    Chapter 6 - Part 2

--- app/app.component.ts
+++ app/app.component.ts
@@ -16,6 +16,7 @@ import { DashboardComponent } from './dashboard.component';
     </nav>
     <router-outlet></router-outlet>
   `,
+  styleUrls: ['app/app.component.css'],
   directives: [ROUTER_DIRECTIVES],
   providers: [
     ROUTER_PROVIDERS,
